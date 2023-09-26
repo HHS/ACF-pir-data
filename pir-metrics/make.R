3465e7c93f71a8e47aae1012b7d0b77ba2d550e2
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2023-09-26 16:29:06 -0500

    add make.R

---
+++ pir-metrics/make.R
@@ -0,0 +1,8 @@
+library(here)
+
+source(here('pir-metrics', 'import', 'src', 'import.R'))
+gc(full = T)
+source(here('process-centers', 'src', 'process-centers.R'))
+gc(full = T)
+source(here('pir-metrics', 'clean', 'src', 'clean.R'))
+gc(full = T)
