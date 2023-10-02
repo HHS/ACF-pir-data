f047fa9defb237895a7599954d4539703c7341eb
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2023-10-02 16:25:51 -0500

    first pass at reporting data quality

--- pir-metrics/make.R
+++ pir-metrics/make.R
@@ -1,8 +1,9 @@
 library(here)
 
-source(here('pir-metrics', 'import', 'src', 'import.R'))
-gc(full = T)
-source(here('process-centers', 'src', 'process-centers.R'))
-gc(full = T)
-source(here('pir-metrics', 'clean', 'src', 'clean.R'))
-gc(full = T)
+# suppressMessages({
+  source(here('pir-metrics', 'import', 'src', 'import.R'))
+  source(here('process-centers', 'src', 'process-centers.R'))
+  gc(full = T)
+  source(here('pir-metrics', 'clean', 'src', 'clean.R'))
+  source(here('pir-metrics', 'reporting', 'src', 'reporting.R'))
+# })
