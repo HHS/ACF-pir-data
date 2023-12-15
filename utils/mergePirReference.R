be48af5c4839d9ec3a25bc532409d6220bdbee15
= <=>
2023-12-15 13:48:22 -0500

    Adjust mergePirReference
    Use list of data frames as first argument
    Add logic for handling missing questions
    Generate unmatched questiona and response tables

--- utils/mergePirReference.R
+++ utils/mergePirReference.R
@@ -4,12 +4,15 @@
 #' create question data frame. Merge question data frame to
 #' appended Section sheets created with appendPirSections.
 #' 
-#' @param response The response data (appended Section sheets).
+#' @param df_list A list of data frames.
 #' @param workbook The workbook that the current data come from.
 #' @examples
 #' mergePirReference(response_df, "<path>/<to>/<workbook>.xlsx")
 
-mergePirReference <- function(response, workbook) {
+mergePirReference <- function(df_list, workbook) {
+  
+  response <- df_list$response
+  question <- df_list$question
   
   # Extract year
   yr <- stringr::str_extract(workbook, "(\\d+).xlsx", group = 1)
@@ -33,10 +36,76 @@ mergePirReference <- function(response, workbook) {
     return(data)
   }
   
-  # Load reference sheet
-  question <- readxl::read_excel(workbook, sheet = "Reference") %>%
-    janitor::clean_names() %>%
-    assert_rows(col_concat, is_uniq, question_number, question_name)
+  missingQuestion <- function(list_of_errors, data) {
+    
+    mi_question_env <- environment()
+    
+    unmatched_response <- data %>%
+      filter(merge != 3)
+    
+    diff_name <- unmatched_response %>%
+      rename(question_name_response = question_name) %>%
+      left_join(
+        question,
+        by = "question_number"
+      ) %>%
+      select(question_number, starts_with("question_name")) %>%
+      rename(question_name_question = question_name) %>%
+      distinct() %>%
+      pipeExpr(
+        assign(
+          "unmatched_questions",
+          unique(.$question_number),
+          envir = mi_question_env
+        )
+      )
+    
+    unmatched_question <- question %>%
+      right_join(
+        select(diff_name, question_number),
+        by = "question_number"
+      )
+    
+    question %>%
+      filter(question_number %notin% unmatched_questions) %>%
+      {assign("question", ., envir = func_env)}
+    
+    logMessage(
+      paste0(
+        "All data for the following variables has been omitted due to",
+        "conflicts in the question name. Please check table unmatched_response",
+        "and table unmatched_question for details about these records",
+        "\n",
+        paste(
+          diff_name$question_number,
+          "-",
+          "Question name in the response table was",
+          diff_name$question_name_response,
+          ".",
+          "Question name in the question table was",
+          diff_name$question_name_question,
+          ".",
+          collapse = "\n"
+        )
+      )
+    )
+    
+    assign(
+      "df_list", 
+      append(
+        df_list, 
+        list(
+          "unmatched_response" = unmatched_response,
+          "unmatched_question" = unmatched_question
+        )
+      ),
+      envir = func_env
+    )
+    
+    data %>%
+      filter(merge == 3) %>%
+      return()
+  }
   
   # Set of unique questions
   question_vars <- unique(question$question_number)
@@ -45,8 +114,18 @@ mergePirReference <- function(response, workbook) {
   response <- response %>%
     # Remove numeric strings added to uniquely identify
     mutate(
-      question_number = gsub("_\\d+$", "", question_number, perl = T)
+      question_number = gsub("_\\d+$", "", question_number, perl = T),
+      q_num_lower = trimws(tolower(question_number)),
+      question_number = ifelse(
+        grepl("^(n/a|n\\.?a\\.?)$", q_num_lower),
+        "na",
+        question_number
+      )
     ) %>%
+    assertr::verify(
+      !any(question_number == "na"),
+      error_fun = naNumber
+    )
     pipeExpr(
       assign("response_vars", unique(.$question_number), envir = func_env)
     ) %>%
@@ -54,7 +133,7 @@ mergePirReference <- function(response, workbook) {
       length(setdiff(response_vars, question_vars)) == 0,
       error_fun = responseMergeError
     ) %>%
-    # Merge to question_name
+    # Merge to get question_name
     left_join(
       attr(response, "text_df"),
       by = c("question_number"),
@@ -62,10 +141,18 @@ mergePirReference <- function(response, workbook) {
     ) %>%
     assertr::verify(!is.na(question_name)) %>%
     # Merge to appended data
-    left_join(
+    left_join_check(
       question,
       by = c("question_number", "question_name"),
       relationship = "many-to-one"
-    )
-  return(list("response" = response, "question" = question))
+    ) %>%
+    assertr::verify(
+      merge == 3,
+      error_fun = missingQuestion
+    ) %>%
+    select(-c(merge))
+    
+  df_list[["question"]] <- question
+  df_list[["response"]] <- response
+  return(df_list)
 }
