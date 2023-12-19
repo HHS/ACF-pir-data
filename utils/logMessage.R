3bd2aab7666aed835e06d8087d948f5f14644250
= <=>
2023-12-19 14:52:48 -0500

    Move creation of log to startLog; document

--- utils/logMessage.R
+++ utils/logMessage.R
@@ -1,12 +1,18 @@
-# Function to log messages to a file
-logMessage <- function(message) {
+#' Log status messages
+#'
+#' @param message The message to be logged.
+#' @param log The log to which to write the message.
+#' @examples
+#' # example code
+#' logMessage("Hello world", "hello_world.txt")
+
+logMessage <- function(message, log) {
   timestamp <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
+  
   log_entry <- paste(timestamp, message, "\n")
-  date <- format(Sys.Date(), "%Y%m%d")
-  logdir <- file.path(logdir, "automated_pipeline_logs")
   cat(
-    log_entry, 
-    file = file.path(logdir, paste0("ingestion_log", "_", date, ".txt")), 
+    log_entry,
+    file = log, 
     append = TRUE
   )
 }
