f1bf9d807902209a917749135cd5265008b86452
= <=>
2023-12-22 16:52:07 -0500

    Initial draft of schema for question matching

---
+++ SQL/question_links_schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS `question_links`;
+use `question_links`;
+
+CREATE TABLE `linked` (
+	uqid varchar(255), 
+	`year` year, 
+	question_id varchar(255), 
+	question_name TEXT, 
+	question_text TEXT, 
+	question_number varchar(55), 
+	category varchar(255),
+	section varchar(2),
+	PRIMARY KEY (uqid, `year`)
+);
+
+CREATE TABLE `unlinked` (
+	`year` year, 
+	question_id varchar(255), 
+	question_name TEXT, 
+	question_text TEXT, 
+	question_number varchar(55), 
+	category varchar(255),
+	section varchar(2),
+	proposed_link TEXT,
+	PRIMARY KEY (question_id, `year`)
+)
