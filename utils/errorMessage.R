ed4e5523b643f0241a28a7c80e97b12625287176
= <=>
2023-12-29 17:35:43 -0500

    Organize ingestion utilities

--- utils/errorMessage.R
+++
@@ -1,6 +0,0 @@
-# Function to log an error message
-errorMessage <- function(error) {
-  error_message <- paste("Error:", conditionMessage(error))
-  logMessage(error_message)
-  stop(error_message)
-}
--- utils/hashVector.R
+++
@@ -1,8 +0,0 @@
-# Function to hash a string vector
-hashVector <- function(string) {
-  hashed <- future_map_chr(
-    string,
-    rlang::hash
-  )
-  return(hashed)
-}
