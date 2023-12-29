8f510367aa7d507b5e87683bc740afeab6b7d596
= <=>
2023-12-29 09:47:44 -0500

    Add schema for logs

---
+++ SQL/log_schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS `pir_logs`;
+use `pir_logs`;
+
+CREATE TABLE `pir_ingestion_logs` (
+    `run` TIMESTAMP,
+    `timestamp` TIMESTAMP,
+    `message` TEXT
+);
+
+CREATE TABLE `mysql_logs` (
+    `timestamp` TIMESTAMP,
+    `message` TEXT
+);
+
+CREATE TABLE `security_logs` (
+    `timestamp` TIMESTAMP,
+    `message` TEXT
+);
