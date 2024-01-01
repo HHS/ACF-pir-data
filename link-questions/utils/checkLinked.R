6a4f2d53363e52e28cf20a6ac794efde00a53d60
= <=>
2023-12-31 19:58:38 -0500

    Correct so uqid are consistent within question_id

--- link-questions/utils/checkLinked.R
+++ link-questions/utils/checkLinked.R
@@ -4,6 +4,19 @@ checkLinked <- function(df_list) {
   linked_db <- df_list$linked_db
   
   if (nrow(linked_db) > 0) {
+    
+    linked <- inner_join(
+      lower_year, 
+      linked_db %>%
+        distinct(question_id, uqid),
+      by = "question_id"
+    )
+    
+    lower_year <- anti_join(
+      lower_year,
+      linked,
+      by = "question_id"
+    )
   
     separated <- cross_join(lower_year, linked_db) %>%
       determineLink() %>%
@@ -11,7 +24,8 @@ checkLinked <- function(df_list) {
     
     unlinked <- separated$unlinked
     linked <- separated$linked %>%
-      distinct(uqid, question_id, .keep_all = T)
+      distinct(uqid, question_id, .keep_all = T) %>%
+      bind_rows(linked)
     
     df_list <- append(df_list, list("linked" = linked, "unlinked" = unlinked))
     
