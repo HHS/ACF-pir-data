4aac0480fec35f6bf677f8bb7868d889db67c682
= <=>
2024-01-04 14:28:00 -0500

    Include linked questions in unlinked question review

--- SQL/question_links/reviewUnlinked.sql
+++ SQL/question_links/reviewUnlinked.sql
@@ -9,10 +9,16 @@ SET @qid_list = (
 	WHERE question_id = qid
 );
 
-SELECT *
+SELECT `year`, question_id, question_number, question_name, question_text, section
 FROM unlinked 
-WHERE JSON_CONTAINS(@qid_list, JSON_ARRAY(question_id));
+WHERE 
+	JSON_CONTAINS(@qid_list, JSON_ARRAY(question_id)) OR
+    question_id = qid
+UNION
+SELECT `year`, question_id, question_number, question_name, question_text, section
+FROM linked
+WHERE JSON_CONTAINS(@qid_list, JSON_ARRAY(question_id))
+;
 
 END //
 DELIMITER ;
-
