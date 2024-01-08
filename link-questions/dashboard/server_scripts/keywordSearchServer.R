023e3c8afe86d7997887624c0455b836d193cc44
= <=>
2024-01-08 14:32:49 -0500

    Error handling for keyword search

--- link-questions/dashboard/server_scripts/keywordSearchServer.R
+++ link-questions/dashboard/server_scripts/keywordSearchServer.R
@@ -1,24 +1,35 @@
 keyword_output <- eventReactive(
   input$keyword_search,
   {
-    dbGetQuery(
-      link_conn,
-      paste0(
-        "call questionKeywordSearch(",
-        "'", input$keyword_table, "',",
-        "'", input$keyword_column, "',", 
-        "'", input$keyword_text, "',",
-        "'", as.numeric(input$keyword_exact), "'",
-        ")"
-      )
-    ) %>%
-      return()
     
-    # df %>%
-    #   kableExtra::kable() %>%
-    #   kableExtra::kable_styling("striped") %>%
-    #   return()
+    if (grepl("\"|\'", input$keyword_text)) {
+      stop("Please enter a string without quotes.")
+    }
+    
+    tryCatch(
+      {
+        dbGetQuery(
+          link_conn,
+          paste0(
+            "call questionKeywordSearch(",
+            "'", input$keyword_table, "',",
+            "'", input$keyword_column, "',", 
+            "'", input$keyword_text, "',",
+            "'", as.numeric(input$keyword_exact), "'",
+            ")"
+          )
+        ) %>%
+          return()
+      },
+      error = function(cnd) {
+        stop(cnd)
+      }
+    )
   }
+  # df %>%
+  #   kableExtra::kable() %>%
+  #   kableExtra::kable_styling("striped") %>%
+  #   return()
 )
 
 output$keyword_output <- renderTable(keyword_output())
