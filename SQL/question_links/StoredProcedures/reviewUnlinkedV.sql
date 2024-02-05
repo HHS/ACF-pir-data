c7b21021614a8b77fc04ed6db4750f738ab3e512
= <=>
2024-02-05 17:00:52 -0500

    Add year to reviewUnlinked

--- SQL/question_links/StoredProcedures/reviewUnlinkedV.sql
+++ SQL/question_links/StoredProcedures/reviewUnlinkedV.sql
@@ -12,24 +12,26 @@ SELECT a.*,
     d.question_number as base_question_number,
     COALESCE(b.question_number, c.question_number) AS comparison_question_number,
 	d.section as base_section,
-    COALESCE(b.section, c.section) AS comparison_section
+    COALESCE(b.section, c.section) AS comparison_section,
+    d.`year` as base_year,
+    COALESCE(b.`year`, c.`year`) AS comparison_year
 FROM (
 	SELECT *
 	FROM unlinked_v
 	WHERE question_id = qid
 ) a
 LEFT JOIN (
-	SELECT DISTINCT question_id, question_name, question_text, question_number, section
-    FROM linked 
+	SELECT DISTINCT question_id, question_name, question_text, question_number, section, JSON_ARRAYAGG(`year`) OVER (PARTITION BY question_id) as `year`
+    FROM linked
 ) b
 ON a.proposed_id = b.question_id
 LEFT JOIN (
-	SELECT DISTINCT question_id, question_name, question_text, question_number, section
+	SELECT DISTINCT question_id, question_name, question_text, question_number, section, JSON_ARRAYAGG(`year`) OVER (PARTITION BY question_id) as `year`
     FROM unlinked 
 ) c
 ON a.proposed_id = c.question_id
 LEFT JOIN (
-	SELECT DISTINCT question_id, question_name, question_text, question_number, section
+	SELECT DISTINCT question_id, question_name, question_text, question_number, section, JSON_ARRAYAGG(`year`) OVER (PARTITION BY question_id) as `year`
     FROM unlinked 
 ) d
 ON a.question_id = d.question_id
