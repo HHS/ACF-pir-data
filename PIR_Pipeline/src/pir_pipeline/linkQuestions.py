97ac02ebfda0d76d4e7e7eb826f7f7a94f0adac6
= <=>
2024-02-16 22:49:52 -0500

    Add script for question linkage

---
+++ PIR_Pipeline/src/pir_pipeline/linkQuestions.py
@@ -0,0 +1,11 @@
+def main():
+    import subprocess, json, os, glob
+    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    config_json = os.path.join(current_dir, "config.json")
+    config = open(config_json)
+    config = json.loads(config.read())
+    script_dir = os.path.join(current_dir, "pir_question_links")
+    script_path = os.path.join(script_dir, "linkQuestions.R")
+    
+    subprocess.call([config["R_Path"], script_path], cwd = script_dir)
