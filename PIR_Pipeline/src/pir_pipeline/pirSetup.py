55417f921ea60ee2619074619da8c6a268ba95c1
= <=>
2024-02-19 10:16:40 -0500

    Create R proj file during setup

--- PIR_Pipeline/src/pir_pipeline/pirSetup.py
+++ PIR_Pipeline/src/pir_pipeline/pirSetup.py
@@ -1,4 +1,5 @@
 def main():
-    from . import createDirectories, configureDB
+    from . import createDirectories, configureDB, createRProj
     createDirectories.main()
     configureDB.main()
+    createRProj.main()
