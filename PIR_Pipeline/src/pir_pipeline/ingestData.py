68b48a3c4fe006420c70631b82aba7ea86d8ef7e
= <=>
2024-02-21 17:25:39 -0500

    Allow immediate ingestion
    Add argparse back in to allow for immediate ingestion from the
    command line with the --now option.

--- PIR_Pipeline/src/pir_pipeline/ingestData.py
+++ PIR_Pipeline/src/pir_pipeline/ingestData.py
@@ -1,6 +1,12 @@
 def main():
     import subprocess, argparse, json, os, glob
     
+    parser = argparse.ArgumentParser(
+        prog="pir-ingest",
+        description="Schedule PIR data for ingestion, or ingest data in Raw folder."
+    )
+    parser.add_argument('--now', action='store_true')
+    
     current_dir = os.path.dirname(os.path.abspath(__file__))
     config_json = os.path.join(current_dir, "config.json")
     config = open(config_json)
@@ -10,6 +16,10 @@ def main():
     schedule_command = 'schtasks /CREATE /TN {} /TR "{}" /SC ONCE /SD {} /ST 01:00'
     
     files = glob.glob(config["Raw"] + "/*")
-    from .pir_watcher import watcher
-    watcher.FolderWatcher(config, schedule_command)
-    # subprocess.call([config["R_Path"], script_path, *files], cwd = current_dir)
+    args = parser.parse_args()
+    
+    if args.now:
+        subprocess.call([config["R_Path"], script_path, *files], cwd = current_dir)
+    else:
+        from .pir_watcher import watcher
+        watcher.FolderWatcher(config, schedule_command)
