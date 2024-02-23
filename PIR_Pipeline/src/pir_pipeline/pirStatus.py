e5f3c5cd6193843ec77225e0c61b61519826c214
= <=>
2024-02-23 13:45:08 -0500

    Add pir status command
    Command to run db queries to check state of ingestion/
    question links.

---
+++ PIR_Pipeline/src/pir_pipeline/pirStatus.py
@@ -0,0 +1,77 @@
+def main():
+    import os, mysql.connector, json, argparse
+    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    config_json = os.path.join(current_dir, "config.json")
+    config = open(config_json)
+    config = json.loads(config.read())
+    
+    db_config = {
+        'host' : config["dbhost"],
+        'port' : config["dbport"],
+        'user' : config['dbusername'],
+        'password' : config['dbpassword'],
+        'database' : 'pir_logs'
+    }
+    
+    parser = argparse.ArgumentParser(
+        prog="pir-status",
+        description="Get status of PIR pipeline."
+    )
+    parser.add_argument('--ingestion', action='store_true')
+    parser.add_argument('--links', action='store_true')
+    
+    args = parser.parse_args()
+    
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor(buffered=True)
+    except Exception as e:
+        print([e._full_msg, e.msg, e.errno])
+    
+    if args.ingestion:
+        query = """
+        SELECT * 
+        FROM pir_ingestion_logs 
+        WHERE run = (
+            SELECT max(run)
+            FROM pir_ingestion_logs
+        )
+        """
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+        print("Most recent ingestion log entries")
+        output = {}
+        i = 0
+        for run, time, message in result:
+            i += 1
+            output[i] = {}
+            output[i]['run'] = run.strftime("%Y/%m/%d, %H:%M:%S")
+            output[i]['time'] = time.strftime("%Y/%m/%d, %H:%M:%S")
+            output[i]['message'] = message
+        print(json.dumps(output, indent=2))
+        
+    if args.links:
+        query = """
+        SELECT * 
+        FROM pir_logs.pir_question_linkage_logs
+        WHERE run = (
+            SELECT max(run)
+            FROM pir_logs.pir_question_linkage_logs
+        )
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+        print("Most recent question link log entries")
+        output = {}
+        i = 0
+        for run, time, message in result:
+            i += 1
+            output[i] = {}
+            output[i]['run'] = run.strftime("%Y/%m/%d, %H:%M:%S")
+            output[i]['time'] = time.strftime("%Y/%m/%d, %H:%M:%S")
+            output[i]['message'] = message
+        print(json.dumps(output, indent=2))
