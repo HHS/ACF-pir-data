d34a33e610861d3715be1bab15b98ef1b2f88241
= <=>
2024-03-04 18:49:31 -0500

    Add renv::activate()

--- PIR_Pipeline/src/pir_pipeline/_common/installation/installPackages.R
+++ PIR_Pipeline/src/pir_pipeline/_common/installation/installPackages.R
@@ -1,6 +1,19 @@
-# Packages
+################################################################################
+## Written by: Reggie Gilliard
+## Date: 02/23/2024
+## Description: Script to restore packages using renv.
+################################################################################
+
+
+# This script takes command line arguments and restores packages using renv.
+
+# Retrieve command line arguments
 args <- commandArgs(TRUE)
+# Extract the path to the renv folder
 renv_path <- args[1]
 
+# Activate the renv environment
 source(file.path(renv_path, "activate.R"))
+# Restore packages
 renv::restore()
+renv::activate()
