34530f52e8aa016bbffc99006a2114cb0e67cebb
skalaga-arch <skalaga@archsystemsinc.com>
2024-03-05 15:43:14 -0500

    commented all python scripts

--- PIR_Pipeline/src/pir_pipeline/startDashboard.py
+++ PIR_Pipeline/src/pir_pipeline/startDashboard.py
@@ -7,18 +7,24 @@ def main():
     config = json.loads(config.read())
     
     script_dir = os.path.join(current_dir, "pir_question_links", "dashboard")
-    
+    # Use subprocess.run to execute the R script that runs the Shiny application.
+    # The R executable path is retrieved from the configuration file.
+    # "-e" flag is used to execute the given expression in R.
     try:
         subprocess.run([config['R_Path'], "-e", "{}".format("shiny::runApp('questionLinkDashboard.R')")], check=True, cwd=script_dir)
     except Exception as e:
+        # In case of an exception, print the traceback information for debugging.
         print(dir(e.__traceback__))
         traceback = e.__traceback__
         while traceback:
             print(traceback.tb_frame)
             print(traceback.tb_lasti)
             print(traceback.tb_lineno)
+            # Move to the next traceback object if it exists.
             try:
                 traceback = traceback.tb_next
             except:
                 traceback = None
+        # Finally, print the command that was attempted to run, along with any output and the return code.
+        # This will help in diagnosing what went wrong with the subprocess call.
         print([e.cmd, e.output, e.returncode])
