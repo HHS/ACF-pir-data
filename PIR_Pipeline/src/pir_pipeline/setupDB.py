cfa41bfc9bd2eca82a77c0f0037b883cdcfe339d
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2024-04-23 13:29:50 -0400

    Remove pir_logs.pir_installation_logs

--- PIR_Pipeline/src/pir_pipeline/setupDB.py
+++ PIR_Pipeline/src/pir_pipeline/setupDB.py
@@ -1,20 +1,6 @@
 # Import necessary modules for database connection, file handling, pattern matching, and timing
 import mysql.connector, os, json, glob, re, time
 
-def log_event(message, db_config):
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor(buffered=True)
-    content = f"""
-    REPLACE INTO pir_logs.pir_installation_logs
-    (timestamp, message)
-    VALUES
-    ("{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}",  "{message}") 
-    """
-    cursor.execute(content)
-    cursor.close()
-    conn.commit()
-    conn.close()
-
 def main():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     config_json = os.path.join(current_dir, "config.json")
@@ -64,16 +50,11 @@ def main():
                 conn.close()
 
                 file_name = re.search(r"(?<=\\|\/)\w+\.sql$", files[0]).group(0)
-                log_event(f"{file_name} successfully executed", db_config)
 
                 files.pop(0)
         except Exception as e:
             # On failure, print error message and rotate the problematic file to the end of the list for a retry
             file_name = re.search(r"(?<=\\|\/)\w+\.sql$", files[0]).group(0)
-            log_event(
-                f"Failed to execute {file_name} with error {e.msg} (code {e.errno}). Placing at the bottom of stack.",
-                db_config
-            )
 
             files.append(files.pop(0))
             retries += 1
