659e76c61da463e916e30ef3bddf210101aeb86a
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-02-25 17:04:35 -0500

    Port PIR cleaning code to python

--- PIR_Pipeline/src/pir_pipeline/pir_ingestion/PIRIngestor.py
+++ PIR_Pipeline/src/pir_pipeline/pir_ingestion/PIRIngestor.py
@@ -1,18 +1,26 @@
+import hashlib
 import os
+import random
 import re
+from typing import Self
 
+import numpy as np
 import pandas as pd
 
+from pir_pipeline.utils import mysql_utils
 from pir_pipeline.utils.paths import INPUT_DIR
 
 
 class PIRIngestor:
-    def __init__(self, workbook: str | os.PathLike):
+    def __init__(self, workbook: str | os.PathLike, db_config: dict):
         """Initialize a PIRIngestor object
 
         Args:
             workbook (str|os.PathLike): File path to an Excel Workbook
         """
+        self._data: dict[pd.DataFrame] = {}
+        self._db_config: dict = db_config
+
         self._workbook = workbook
 
     def make_snake_name(self, name: str) -> str:
@@ -27,18 +35,66 @@ class PIRIngestor:
         df = df.groupby(columns).sample(1).reset_index().drop(columns="index")
         return df
 
-    def get_section(self, question_number: str):
-        return re.search("^([A-Z])", question_number).group(1)
+    def missing_question_error(
+        self, response: pd.DataFrame, question: pd.DataFrame, missing_questions: set
+    ) -> pd.DataFrame:
+        response = (
+            response[["question_number", "question_name", "section"]][
+                response["question_number"].isin(missing_questions)
+            ]
+            .groupby(["question_number", "question_name"])
+            .sample(1)
+        )
+        question = pd.concat([question, response])
 
-    def extract_sheets(self):
+        return question
+
+    def get_section(self, question_number: str) -> str:
+        if not isinstance(question_number, str):
+            return ""
+
+        section = re.search("^([A-Z])", question_number)
+        if section:
+            return section.group(1)
+
+        return ""
+
+    def hash_columns(self, row: pd.Series) -> str:
+        random.seed(4298)
+        string = "".join([str(item) for item in row])
+        byte_string = string.encode()
+
+        return hashlib.md5(byte_string).hexdigest()
+
+    def make_db_connections(self) -> Self:
+        self._connections = mysql_utils.make_db_connections(**self._db_config)
+
+        return self
+
+    def close_db_connections(self) -> Self:
+        for connection in self._connections.values():
+            connection.close()
+
+        return self
+
+    def get_schemas(self) -> Self:
+        schemas = mysql_utils.get_schemas(
+            self._connections["pir_data"], ["question", "response", "program"]
+        )
+        for schema, array in schemas.items():
+            schemas[schema] = pd.DataFrame.from_records(array[1:], columns=array[0])
+
+        self._schemas = schemas
+        return self
+
+    def extract_sheets(self) -> Self:
         """Load the workbook, and extract sheets and year."""
         self._year = re.search(r"(\d{4})\.(csv|xlsx?)$", self._workbook).group(1)
         self._workbook = pd.ExcelFile(self._workbook)
         self._sheets = self._workbook.sheet_names
         return self
 
-    def load_data(self):
-        self._data = {}
+    def load_data(self) -> Self:
         for sheet in self._sheets:
             name = self.make_snake_name(sheet)
             section_condition = sheet.find("Section") > -1
@@ -64,7 +120,7 @@ class PIRIngestor:
                     # Needed for the merge of question name.
                     duplicated_names[column_name] += 1
                     column_names.append(
-                        column_name + f"_{duplicated_names[column_name]}"
+                        str(column_name) + f"_{duplicated_names[column_name]}"
                     )
 
                 df.columns = column_names
@@ -112,13 +168,17 @@ class PIRIngestor:
                     ), f"Some observations are still duplicated:\n {df[df[unique_columns].duplicated()]}"
 
                 df["section"] = df["question_number"].map(self.get_section)
+                name = "question"
+
+            elif program_condition:
+                name = "program"
 
             df.columns = df.columns.map(self.make_snake_name)
             self._data[name] = df
 
         return self
 
-    def append_sections(self):
+    def append_sections(self) -> Self:
         df_list = []
         to_delete = []
         for name, df in self._data.items():
@@ -133,41 +193,65 @@ class PIRIngestor:
 
         return self
 
-    def merge_response_question(self):
+    def merge_response_question(self) -> Self:
         # Response data
-        def gen_lower_question_number(string: str):
-            string = string.lower().strip()
-            return "na" if re.search(r"^n/a|n\\.?a\\.?$", string) else string
+        def gen_question_number(string: str):
+            if not isinstance(string, str):
+                return string
+
+            string = re.sub(r"_\d+$", "", string)
+
+            if string == "nan":
+                return np.nan
+
+            return string
 
         response = self._data["response"]
         response["question_number"] = response["question_number"].map(
-            lambda x: re.sub(r"_\d+$", "", x)
-        )
-        response["q_num_lower"] = response["question_number"].map(
-            gen_lower_question_number
+            gen_question_number
         )
 
         # Question data
-        question = self._data["reference"]
-        self._data["question"] = self._data["reference"]
-        del self._data["reference"]
+        question = self._data["question"]
 
         # Confirm all questions are in question
         missing_questions = set(response["question_number"]) - set(
             question["question_number"]
         )
-        assert (
-            missing_questions == set()
-        ), f"Some questions are missing: {missing_questions}"
+        try:
+            assert missing_questions == set()
+        except AssertionError:
+            question = self.missing_question_error(
+                response, question, missing_questions
+            )
+            missing_questions = set(response["question_number"]) - set(
+                question["question_number"]
+            )
+            assert (
+                missing_questions == set()
+            ), f"Some questions are missing: {missing_questions}"
 
         # Merge
         response = response.merge(
-            question,
+            question.drop(columns=["type"]),
             how="left",
             on=["question_number", "question_name"],
             validate="many_to_one",
             indicator=True,
         )
+        assert (
+            not response[
+                [
+                    "grant_number",
+                    "program_number",
+                    "type",
+                    "question_number",
+                    "question_name",
+                ]
+            ]
+            .duplicated()
+            .any()
+        ), "Some records are duplicated"
 
         for column in response.columns.tolist():
             if column.endswith("_x"):
@@ -182,9 +266,83 @@ class PIRIngestor:
 
         return self
 
+    def clean_pir_data(self) -> Self:
+        def get_region(region: str):
+            region = re.sub(r"\D+", "", region)
+            if region:
+                return int(region)
+
+            return None
+
+        self.make_db_connections().get_schemas()
+        uid_columns = ["grant_number", "program_number", "program_type"]
+        qid_columns = ["question_number", "question_name"]
+
+        # Clean response data
+        response = self._data["response"]
+        response.rename(columns={"type": "program_type"}, inplace=True)
+        duplicates = response[response[uid_columns + qid_columns].duplicated()]
+        assert duplicates.empty, f"Some duplicated records:\n{duplicates}"
+        response["uid"] = response[uid_columns].apply(self.hash_columns, axis=1)
+        response["question_id"] = response[qid_columns].apply(self.hash_columns, axis=1)
+
+        # Program
+        program = self._data["program"]
+        program.rename(
+            columns={
+                "program_zip1": "program_zip_code",
+                "program_zip2": "program_zip_4",
+                "program_phone": "program_main_phone_number",
+                "program_email": "program_main_email",
+            },
+            inplace=True,
+        )
+        duplicates = program[program[uid_columns].duplicated()]
+        assert duplicates.empty, f"Some duplicated records:\n{duplicates}"
+        program["uid"] = program[uid_columns].apply(self.hash_columns, axis=1)
+        program["region"] = program["region"].map(get_region)
+
+        # Question
+        question = self._data["question"]
+        question.rename(columns={"type": "question_type"}, inplace=True)
+        duplicates = question[question[qid_columns].duplicated()]
+        assert duplicates.empty, f"Some duplicated records:\n{duplicates}"
+        question["question_id"] = question[qid_columns].apply(self.hash_columns, axis=1)
+
+        # Add year, subset to relevant variables only
+        for frame in ["response", "program", "question"]:
+            final_columns = self._schemas[frame]["Field"]
+            df = eval(frame)
+            df["year"] = self._year
+            missing_variables = set(final_columns) - set(df.columns)
+            for var in missing_variables:
+                df[var] = None
+            df = df[final_columns]
+            self._data[frame] = df
+
+        self.close_db_connections()
+
     def ingest(self):
-        self.extract_sheets().load_data().append_sections().merge_response_question()
+        (
+            self.extract_sheets()
+            .load_data()
+            .append_sections()
+            .merge_response_question()
+            .clean_pir_data()
+        )
 
 
 if __name__ == "__main__":
-    PIRIngestor(os.path.join(INPUT_DIR, "pir_export_2023.xlsx")).ingest()
+    from pir_pipeline.config import db_config
+
+    db_config.update({"databases": ["pir_data"]})
+    files = os.listdir(INPUT_DIR)
+    for file in files:
+        year = re.search(r"\d{4}", file).group(0)
+        year = int(year)
+        if year < 2008:
+            continue
+        elif year == 2008 and file.endswith(".xlsx"):
+            continue
+
+        PIRIngestor(os.path.join(INPUT_DIR, file), db_config).ingest()
