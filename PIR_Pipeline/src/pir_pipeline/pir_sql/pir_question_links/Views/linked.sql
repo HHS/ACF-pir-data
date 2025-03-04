50ecd79cccb1537efeb0885b815add35a59945ac
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-03-04 10:30:59 -0500

    Remove defunct files

--- PIR_Pipeline/src/pir_pipeline/pir_sql/pir_question_links/Views/linked.sql
+++
@@ -1,9 +0,0 @@
--- =============================================
--- Author:      Reggie Gilliard
--- Create date: 03/01/2024
--- Description: This view provides a list of questions that have been linked.
--- =============================================
-CREATE OR REPLACE VIEW pir_question_links.linked_v AS
-SELECT *
-FROM pir_question_links.linked
-;
