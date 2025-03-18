a57c68ba47ac11cf4612bbd35f50723ed9dfeb52
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-03-18 13:50:46 -0400

    Add module for data retrieval

---
+++ PIR_Pipeline/src/pir_pipeline/dashboard/data.py
@@ -0,0 +1,74 @@
+__all__ = ["get_review_data"]
+
+from sqlalchemy import func, select
+
+from pir_pipeline.utils.SQLAlchemyUtils import SQLAlchemyUtils
+from pir_pipeline.utils.utils import clean_name
+
+
+def get_review_data(review_type: str, db: SQLAlchemyUtils):
+    if review_type == "unlinked":
+        table = db._tables["unlinked"]
+        query = select(table)
+    elif review_type == "intermittent":
+        table = db._tables["question"]
+        year_query = select(func.count(func.distinct(table.c.year))).scalar_subquery()
+        uqid_query = (
+            select(table.c.uqid)
+            .group_by(table.c.uqid)
+            .having(func.count(table.c.uqid) < year_query)
+            .subquery()
+        )
+        query = (
+            select(
+                table.c.uqid,
+                table.c.question_name,
+                table.c.question_number,
+                table.c.question_text,
+                table.c.section,
+                table.c.question_type,
+            )
+            .where(table.c.uqid.in_(uqid_query))
+            .distinct()
+        )
+    else:
+        table = db._tables["linked"]
+        subquery = select(table.c.question_id, table.c.uqid).distinct().subquery()
+        right = (
+            select(subquery.c.uqid)
+            .group_by(subquery.c.uqid)
+            .having(func.count(subquery.c.question_id) > 1)
+            .subquery()
+        )
+        query = (
+            select(
+                table.c.uqid,
+                table.c.question_name,
+                table.c.question_number,
+                table.c.question_text,
+                table.c.section,
+                table.c.question_type,
+            )
+            .join(right, table.c.uqid == right.c.uqid)
+            .distinct()
+        )
+
+    with db.engine.connect() as conn:
+        result = conn.execute(query)
+        data = db.to_dict(result.all(), query.c.keys())
+
+    columns = [clean_name(col, "title") for col in query.c.keys()]
+    data.insert(0, columns)
+
+    return data
+
+
+def get_matches(payload: dict, db: SQLAlchemyUtils):
+    review_type = payload["review-type"]
+
+    if review_type == "unlinked":
+        question = db.tables["question"]
+    elif review_type == "intermittent":
+        pass
+    elif review_type == "inconsistent":
+        pass
