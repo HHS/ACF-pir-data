23cbec3581c027fbc734743e037246180f1ef87f
unknown <reggie.gilliard@EC2AMAZ-5LQS4RO.acf.hhs.local>
2025-03-23 02:14:57 +0000

    Add tests for gen_engine, validate_table, get_columns

--- PIR_Pipeline/tests/utils/test_SQLAlchemyUtils.py
+++ PIR_Pipeline/tests/utils/test_SQLAlchemyUtils.py
@@ -1,18 +1,56 @@
+import pytest
+from sqlalchemy import select, text
+
+from pir_pipeline.config import db_config
+from pir_pipeline.utils.SQLAlchemyUtils import SQLAlchemyUtils
+
+
+@pytest.mark.usefixtures("create_database")
 class TestSQLAlchemyUtils:
     def test_gen_engine(self):
-        pass
+        db_config.update({"username": db_config["user"]})
+        db_config.pop("user")
+        query = select(text("'Connection Made'"))
+        with SQLAlchemyUtils.__new__(SQLAlchemyUtils).gen_engine(
+            **db_config, database="pir_test", drivername="postgresql+psycopg"
+        )._engine.connect() as conn:
+            result = conn.execute(query)
+            value = result.first()
 
+        assert value[0] == "Connection Made", "Incorrect value."
+
+    # How to test this and drop_db when it is in the fixture?
     def test_create_db(self):
         pass
 
     def test_drop_db(self):
         pass
 
-    def test_validate_table(self):
-        pass
+    def test_validate_table(self, sql_utils):
+        for table in ["invalid1", "drop table response", "invalid_2"]:
+            with pytest.raises(AssertionError):
+                sql_utils.validate_table(table)
 
-    def test_get_columns(self):
-        pass
+    def test_get_columns(
+        self, sql_utils, question_columns, response_columns, program_columns
+    ):
+        # Get all columns from a table
+        correct = {
+            "program": program_columns,
+            "question": question_columns,
+            "response": response_columns,
+        }
+        for table in ["program", "question", "response"]:
+            columns = sql_utils.get_columns(table)
+            assert (
+                set(columns) == correct[table]
+            ), f"Columns are incorrect for table: {table}"
+            assert isinstance(columns, list), "Columns are not in a list"
+
+        # Get a subset
+        columns = sql_utils.get_columns("program", "AND column_name LIKE '%zip%'")
+        assert set(columns) == {"program_zip1", "program_zip2"}
+        assert isinstance(columns, list), "Columns are not in a list"
 
     def test_get_records(self):
         pass
@@ -25,3 +63,7 @@ class TestSQLAlchemyUtils:
 
     def test_to_dict(self):
         pass
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-sk", "test_gen_engine"])
