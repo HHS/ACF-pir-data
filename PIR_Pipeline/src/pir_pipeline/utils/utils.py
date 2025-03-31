3e50c99ff5870437999fcd012d0fe9e5c43558eb
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-03-31 10:42:08 -0400

    Docstrings for utils.py

--- PIR_Pipeline/src/pir_pipeline/utils/utils.py
+++ PIR_Pipeline/src/pir_pipeline/utils/utils.py
@@ -5,6 +5,14 @@ import re
 
 
 def get_searchable_columns(columns: list[str]) -> list[str]:
+    """Return non-identifying columns formatted for external audiences
+
+    Args:
+        columns (list[str]): A list of column names
+
+    Returns:
+        list[str]: Formatted column names
+    """
     keep_columns = []
     for column in columns:
         if not column.endswith("id") and column not in ["year"]:
@@ -14,7 +22,15 @@ def get_searchable_columns(columns: list[str]) -> list[str]:
     return keep_columns
 
 
-def get_logger(name: str):
+def get_logger(name: str) -> logging.Logger:
+    """Return a logger
+
+    Args:
+        name (str): Name for the logger
+
+    Returns:
+        Logger: An instance of logging.Logger
+    """
     logging.basicConfig(
         format="%(asctime)s|%(name)s|%(levelname)s|%(message)s",
         level=logging.DEBUG,
