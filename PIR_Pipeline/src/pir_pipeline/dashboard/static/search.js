2f6413359bb2bedb420898fafd841e0425cd171c
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-04-03 08:36:36 -0400

    Remove flashed messages when a successful search occurs

--- PIR_Pipeline/src/pir_pipeline/dashboard/static/search.js
+++ PIR_Pipeline/src/pir_pipeline/dashboard/static/search.js
@@ -2,12 +2,18 @@ import { updateTable} from "./utilities.js";
 
 const searchForm = document.getElementById("search-form")
 searchForm.addEventListener("submit", async (e) => {
+    // Flash an error if keyword is not provided
     if (!document.getElementById("keyword-search").value) {
         let data = {
             "error": "Missing keyword"
         }
         fetch("/search", {"method": "POST", "headers": {"Content-type": "application/json"}, "body": JSON.stringify(data)})
         return;
+    }
+    // Removed flashed messages if successful
+    const flashedDivs = document.getElementsByClassName("flash");
+    for (let i = 0; i < flashedDivs.length; i++) {
+        flashedDivs[i].remove();
     }
     e.preventDefault();
     updateTable(e);
