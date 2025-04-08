01b35c5b067f7e9ab171e2287593eacdb21185f2
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-04-08 17:03:56 -0400

    Correct tests

--- PIR_Pipeline/tests/utils/test_SQLAlchemyUtils.py
+++ PIR_Pipeline/tests/utils/test_SQLAlchemyUtils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from sqlalchemy import bindparam, select, text
 
@@ -94,7 +95,7 @@ class TestSQLAlchemyUtilsNoData:
             assert record_count == self.validation[table]
 
 
-@pytest.mark.usefixtures("inserted")
+@pytest.mark.usefixtures("inserted", "create_database")
 class TestSQLAlchemyUtilsData:
     def test_get_records(self, db_columns, sql_utils):
         queries = [
@@ -105,7 +106,7 @@ class TestSQLAlchemyUtilsData:
             ),
             (
                 "SELECT * FROM question WHERE section = 'A'",
-                self.validation["question"] / 4,
+                (self.validation["question"] / 4) - 1,
                 db_columns["question"],
             ),
             (
@@ -126,6 +127,7 @@ class TestSQLAlchemyUtilsData:
         assert set(uqids) == {"1"}, f"Incorrect uqids: {set(uqids)}"
 
         section_a = sql_utils.get_records("SELECT * FROM question WHERE section = 'A'")
+        section_a.replace({np.nan: None}, inplace=True)
         section_a.rename(columns={"question_id": "qid"}, inplace=True)
         section_a["uqid"] = "A"
         records = section_a.to_dict(orient="records")
@@ -161,4 +163,4 @@ class TestSQLAlchemyUtilsData:
 
 
 if __name__ == "__main__":
-    pytest.main([__file__, "-sk", "test_insert_records"])
+    pytest.main([__file__, "-sk", "test_update_records"])
