51c167c5bd9dca7e2612e2aa392d990fd9fbfe83
Gilliard Reggie <reggie.gilliard@ohs-dv-adm-rg-l.acf.hhs.local>
2025-04-11 12:01:18 -0400

    SAM: Connect to local db

--- pir-ingestor-lambda/pir_ingestor/app.py
+++ pir-ingestor-lambda/pir_ingestor/app.py
@@ -1,19 +1,29 @@
 import json
+import os
 import urllib.parse
 
 import boto3
 
 from pir_pipeline.ingestion.PIRIngestor import PIRIngestor
+from pir_pipeline.utils.SQLAlchemyUtils import SQLAlchemyUtils
 
 s3 = boto3.client('s3')
+db_config = {
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "database": "pir"
+}
+sql_utils = SQLAlchemyUtils(**db_config, drivername="postgresql+psycopg")
 
 def lambda_handler(event, context):
+    print(db_config)
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        print(f"CONTENT TYPE: {response["ContentType"]}")
-        print("IT WORKED")
+        PIRIngestor(key, sql_utils).ingest()
         return response["ContentType"]
     except Exception as e:
         print(e)
