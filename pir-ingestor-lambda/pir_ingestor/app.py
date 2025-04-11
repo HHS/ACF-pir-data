87f5dcacb25d0256600465e1a6c47d937a063b37
Gilliard Reggie <reggie.gilliard@ohs-dv-adm-rg-l.acf.hhs.local>
2025-04-11 09:33:04 -0400

    Continue testing pir ingestor lambda

--- pir-ingestor-lambda/pir_ingestor/app.py
+++ pir-ingestor-lambda/pir_ingestor/app.py
@@ -1,5 +1,21 @@
+import json
+import urllib.parse
+
+import boto3
+
 from pir_pipeline.ingestion.PIRIngestor import PIRIngestor
 
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    pass
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print(f"CONTENT TYPE: {response["ContentType"]}")
+        print("IT WORKED")
+        return response["ContentType"]
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
