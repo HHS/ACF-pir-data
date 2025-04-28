aa1a2133354841f55591c03f85411e08818f30cf
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-04-28 14:24:21 -0400

    Add functionality to write to db on commit changes clicked; remove content from modal when closed

--- PIR_Pipeline/src/pir_pipeline/dashboard/static/search/search.js
+++ PIR_Pipeline/src/pir_pipeline/dashboard/static/search/search.js
@@ -1,13 +1,15 @@
 import { updateTable, rowToJSON, updateFlashcardTables, storeLink } from "../utilities.js";
 
-const searchForm = document.getElementById("search-form")
+const searchForm = document.getElementById("search-form");
+const searchModal = document.getElementById("search-modal");
+
 searchForm.addEventListener("submit", async (e) => {
     // Flash an error if keyword is not provided
     if (!document.getElementById("keyword-search").value) {
         let data = {
             "error": "Missing keyword"
         }
-        fetch("/search", {"method": "POST", "headers": {"Content-type": "application/json"}, "body": JSON.stringify(data)})
+        fetch("/search", { "method": "POST", "headers": { "Content-type": "application/json" }, "body": JSON.stringify(data) })
         return;
     }
     // Removed flashed messages if successful
@@ -26,13 +28,56 @@ function getFlashcardData(e) {
     const row = element.closest('tr');
     const rowRecord = rowToJSON(row);
 
-    document.getElementById("search-modal").setAttribute("open", "true");
-    document.getElementById("search-modal").removeAttribute("hidden");
-    
-    fetch("/search/data", { "method": "POST", "headers": {"Content-type": "application/json"}, "body": JSON.stringify(rowRecord)})
+    searchModal.setAttribute("open", "true");
+    searchModal.removeAttribute("hidden");
+
+    fetch("/search/data", { 
+        "method": "POST", 
+        "headers": { "Content-type": "application/json" }, 
+        "body": JSON.stringify(rowRecord) 
+    })
         .then(response => response.json())
         .then(data => updateFlashcardTables(data))
 }
 
+function commitChanges(e) {
+    e.preventDefault();
+    // Commit changes
+    const payload = {
+        "action": "finalize",
+        "data": ""
+    }
+
+    fetch("/review/link", {
+        "method": "POST",
+        "headers": { "Content-type": "application/json" },
+        "body": JSON.stringify(payload)
+    })
+
+    // Close modal
+    searchModal.removeAttribute("open");
+}
+
+// Observe the modal and remove content if it is closed
+// Adapted from https://stackoverflow.com/questions/41424989/javascript-listen-for-attribute-change
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.type === "attributes") {
+            if (mutation.attributeName == "open" && mutation.oldValue == "true") {
+                const tables = searchModal.getElementsByTagName("table");
+                for (let i = 0; i < tables.length; i++) {
+                    tables[i].innerHTML = ""
+                }
+            }
+        }
+    })
+})
+
+observer.observe(searchModal, {
+    attributes: true,
+    attributeOldValue: true
+})
+
 document.getFlashcardData = getFlashcardData;
 document.storeLink = storeLink;
+document.commitChanges = commitChanges;
