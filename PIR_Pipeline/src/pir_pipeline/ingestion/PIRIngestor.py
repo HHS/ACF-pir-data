25b8e773917eb0687e163cb81015ac15c7622944
skalaga-arch <skalaga@archsystemsinc.com>
2025-05-02 12:19:22 -0400

    adding script for search funcitonality

--- PIR_Pipeline/src/pir_pipeline/ingestion/PIRIngestor.py
+++ PIR_Pipeline/src/pir_pipeline/ingestion/PIRIngestor.py
@@ -699,8 +699,8 @@ class PIRIngestor:
             for record in initial_records:
                 cleaned = model.model_validate(record).model_dump()
                 cleaned_records.append(cleaned)
-            if table == "response":
-                    continue
+            # if table == "response":
+            #         continue
             self._sql.insert_records(cleaned_records, table)
 
         self._logger.info(f"Data inserted for {self._year}")
