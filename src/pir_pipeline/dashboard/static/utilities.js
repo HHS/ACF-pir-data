61f11f7773a6c6ec45cd07a0ec01dd320362f767
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-05-05 16:02:52 -0400

    utilities: Edit and document JS utility functions

--- src/pir_pipeline/dashboard/static/utilities.js
+++ src/pir_pipeline/dashboard/static/utilities.js
@@ -1,4 +1,9 @@
 // Logic adapted from Gemini
+/**
+ * Return the correct svg image for a linking element
+ * 
+ * @param {*} button 
+ */
 function getLinkingSVG(button) {
     if (button.value == "unlink") {
         fetch('/static/images/Close--large.svg')
@@ -30,6 +35,11 @@ function getLinkingSVG(button) {
     }
 }
 
+/**
+ * Update the table related to the present event
+ * 
+ * @param {*} event The event triggering the table update
+ */
 function updateTable(event) {
     // Get the form data
     const form = event.srcElement;
@@ -39,6 +49,7 @@ function updateTable(event) {
     const tables = document.getElementsByTagName("table");
     const table = tables[0];
 
+    // Make a post request to the current page
     fetch(document.URL, { "method": "POST", "body": formData })
         .then(response => response.json())
         .then(data => {
@@ -50,6 +61,13 @@ function updateTable(event) {
         });
 }
 
+/**
+ * Populate the target table with the provided data
+ * 
+ * @param {*} data JSON formatted data to use to populate the table
+ * @param {*} table The table to build/update
+ * @returns An HTML table element
+ */
 function buildTable(data, table) {
     // Constant buttons
     const expandButtonBase = document.createElement("button");
@@ -62,19 +80,12 @@ function buildTable(data, table) {
     accordionDivBase.className = "accordion";
 
     const linkButtonBase = document.createElement("button");
-    // linkButtonBase.className = "btn btn-primary";
     linkButtonBase.setAttribute("onclick", "storeLink(event)");
-
-    // Other constants
-    let reviewType = document.getElementById("review-type");
-    if (reviewType) {
-        reviewType = reviewType.value;
-    }
 
     // Set table header row
     table.innerHTML = '';
 
-    let header = data[0];
+    let header = data[0]; // List of column names
     let head = document.createElement("thead");
     let headerRow = document.createElement("tr");
     head.appendChild(headerRow)
@@ -90,12 +101,14 @@ function buildTable(data, table) {
     headerRow.appendChild(actionsColumn);
     table.appendChild(head);
 
+    // Populating the table body
     let body = document.createElement("tbody");
 
     for (let i = 1; i < data.length; i++) {
         const row = document.createElement("tr");
 
-        let row_data = data[i];
+        // Add data to the table
+        let row_data = data[i]; // Table entry
         for (let key in row_data) {
             const cell = document.createElement("td");
             cell.innerHTML = row_data[key];
@@ -103,67 +116,19 @@ function buildTable(data, table) {
             row.appendChild(cell);
         }
 
-        if (table.id == "review-results-table") {
-            const divID = table.id + "-div-" + i;
-            const trID = table.id + "-tr-" + i;
-            const tdID = table.id + "-td-" + i;
-            const expandButton = expandButtonBase.cloneNode(true);
-            expandButton.innerHTML = "";
-            expandButton.setAttribute("data-bs-target", `#collapse-${trID}`);
-            expandButton.setAttribute("aria-controls", `collapse-${trID}`);
-            expandButton.setAttribute("onclick", `getQuestionData(event, '${reviewType}')`);
-            const accordionDiv = accordionDivBase.cloneNode(true);
-            accordionDiv.appendChild(expandButton);
-
-            const actionsCell = document.createElement("td");
-            actionsCell.appendChild(accordionDiv);
-            row.appendChild(actionsCell);
-
-            // Create div to hold table
-            const div = document.createElement("div");
-            div.id = `collapse-${divID}`;
-
-            // Create row and cell to hold div
-            // Adapted from Claude logic, needed to maintain alignment
-            var tr = document.createElement("tr");
-            tr.className = "accordion-collapse collapse";
-            tr.id = `collapse-${trID}`;
-            const td = document.createElement("td");
-            td.id = `collapse-${tdID}`;
-            td.setAttribute("colspan", data.length);
-
-            td.appendChild(div);
-            tr.appendChild(td);
-        }
-        else if (table.id.includes("proposed-matches-table")) {
-            const actionsCell = document.createElement("td");
-            if (["unlinked", "intermittent"].find(item => item == reviewType)) {
-                const linkButton = linkButtonBase.cloneNode(true);
-                linkButton.value = "link";
-                getLinkingSVG(linkButton);
-                actionsCell.append(linkButton);
-            }
-            else if (reviewType == "inconsistent") {
-                const unlinkButton = linkButtonBase.cloneNode(true);
-                unlinkButton.value = "unlink";
-                getLinkingSVG(unlinkButton);
-                actionsCell.append(unlinkButton);
-            }
-
-
-            row.append(actionsCell);
-        }
-
         body.appendChild(row);
-        if (table.id == "review-results-table") {
-            body.appendChild(tr);
-        }
     }
 
     table.appendChild(body);
     return table;
 }
 
+/**
+ * Convert a table row to JSON
+ * 
+ * @param {*} row A table row to convert to JSON
+ * @returns JSON object containing data from a table row
+ */
 function rowToJSON(row) {
     const cells = row.getElementsByTagName("td");
     let record = {}
@@ -176,39 +141,13 @@ function rowToJSON(row) {
     return record
 }
 
-function getQuestionData(event, reviewType) {
-    const row = event.srcElement.closest("tr");
-    const button = event.srcElement
-    const tr = document.getElementById(button.getAttribute("aria-controls"));
-    const div = document.getElementById(tr.id.replace("-tr-", "-div-"));
-
-    // Exit if the target div already contains a table (i.e. matches have been found once).
-    let table = div.getElementsByTagName("table");
-    if (table.length > 0) {
-        return
-    }
-
-    let record = rowToJSON(row);
-    let payload = {
-        "review-type": reviewType,
-        "record": record
-    }
-
-    fetch("/review/match", { "method": "POST", "headers": { "Content-type": "application/json" }, "body": JSON.stringify(payload) })
-        .then(response => response.json())
-        .then(data => fillMatchDiv(div, data));
-}
-
-function fillMatchDiv(div, data) {
-    const table = document.createElement("table");
-    const idNumber = div.id.replace("collapse-review-results-table-div-", "")
-    table.className = "table table-hover";
-    table.setAttribute("id", `proposed-matches-table-${idNumber}`);
-    div.setAttribute("style", "width: 100%");
-    div.appendChild(table);
-    buildTable(data, table);
-}
-
+/**
+ * Build the search table
+ * 
+ * @param {*} data The data to use to build the search table
+ * @param {*} table An HTML table to fill with content
+ * @returns An HTML table element
+ */
 function buildSearchTable(data, table = document.createElement("table")) {
     // Constant buttons
     const expandButtonBase = document.createElement("button");
@@ -243,8 +182,10 @@ function buildSearchTable(data, table = document.createElement("table")) {
     headerRow.appendChild(actionsColumn);
     table.appendChild(head);
 
+    // Build table body
     let body = document.createElement("tbody");
     let record_num = 0;
+
     for (let key in data) {
         if (key == "columns") {
             continue
@@ -252,7 +193,6 @@ function buildSearchTable(data, table = document.createElement("table")) {
 
         const expandButton = expandButtonBase.cloneNode(true);
         expandButton.innerHTML = "";
-
 
         const accordionDiv = accordionDivBase.cloneNode(true);
 
@@ -277,6 +217,7 @@ function buildSearchTable(data, table = document.createElement("table")) {
             const trID = table.id + "-tr-" + record_num + "-" + i;
             expandButton.setAttribute("data-bs-target", `tr[id*="${table.id}-tr-${record_num}-"]`);
 
+            // If not the first row, this row should be hidden/collapsible
             if (i > 0) {
                 row.className = `accordion-collapse collapse collapsible-row-${record_num}`;
                 row.id = `collapse-${trID}`;
@@ -289,15 +230,19 @@ function buildSearchTable(data, table = document.createElement("table")) {
                 }
                 expandButton.setAttribute("aria-controls", expandValue);
             }
+            // Otherwise, it is the header-row
             else {
                 row.setAttribute("onclick", "expandContractRow(event)");
 
                 if (row_data["year"].match(",|-")) {
                     accordionDiv.appendChild(expandButton);
                 }
+
                 actionsCell.appendChild(accordionDiv);
+
                 const reviewButton = reviewButtonBase.cloneNode(true);
                 getLinkingSVG(reviewButton);
+                
                 actionsCell.appendChild(reviewButton);
                 row.appendChild(actionsCell);
             }
@@ -305,11 +250,19 @@ function buildSearchTable(data, table = document.createElement("table")) {
         }
         record_num += 1
     }
+
     table.appendChild(body);
     return table
 }
 
+/**
+ * Update the flashcard tables
+ * 
+ * @param {*} data The data to use to update the table
+ * @returns 
+ */
 function updateFlashcardTables(data) {
+    // Update the main question table (the question being edited)
     const questionTable = document.getElementById("flashcard-question-table");
     const tables = {}
 
@@ -322,6 +275,7 @@ function updateFlashcardTables(data) {
     question.className = "table table-hover";
     tables["question"] = question.outerHTML;
 
+    // Update the matches table
     const matchesTable = document.getElementById("flashcard-matches-table");
 
     if (Object.keys(data["matches"]).length === 0) {
@@ -348,6 +302,12 @@ function updateFlashcardTables(data) {
     return Promise.resolve(tables)
 }
 
+/**
+ * 
+ * @param {*} data The data to use to populate the table
+ * @param {*} table The table to update
+ * @returns 
+ */
 function buildReviewTable(data, table = document.createElement("table")) {
     // Constant buttons
     const expandButtonBase = document.createElement("button");
@@ -417,6 +377,7 @@ function buildReviewTable(data, table = document.createElement("table")) {
 
             const linkButton = linkButtonBase.cloneNode(true);
 
+            // If not the first row, this row should be hidden/collapsible
             if (i > 0) {
                 row.className = `accordion-collapse collapse collapsible-row-${record_num}`;
                 row.id = `collapse-${trID}`;
@@ -435,6 +396,7 @@ function buildReviewTable(data, table = document.createElement("table")) {
                     actionsCell.appendChild(linkButton);
                 }
             }
+            // Otherwise, it is the header-row
             else {
                 row.id = `collapse-${table.id}-tr-${record_num}`
 
@@ -455,20 +417,23 @@ function buildReviewTable(data, table = document.createElement("table")) {
         }
         record_num += 1
     }
+
     table.appendChild(body);
     return table
 }
 
-// Store link logic should be updated on search page because
-// the links should be ephemeral and should update as unlink and link
-// are clicked within a modal. Changes should be committed immediately
-// on clicking confirm changes. 
+/**
+ * Store linking actions made by the user within the session
+ * 
+ * @param {*} event The event that triggered storeLink
+ */
 function storeLink(event) {
     let button = event.target;
     if (button.tagName != "BUTTON") {
         button = button.closest("button");
     }
 
+    // Get the records to be matched
     const matchesTable = document.getElementById("flashcard-matches-table");
     const matchRow = button.closest("tr");
 
@@ -486,6 +451,7 @@ function storeLink(event) {
         "match_question_id": matchRecord.question_id
     }
 
+    // Store the link
     const payload = {
         "action": "build",
         "data": linkDetails
@@ -499,15 +465,22 @@ function storeLink(event) {
         "body": JSON.stringify(payload)
     });
 
+    // Move the row(s) involved in the match to the questionTable/matchesTable
     const matchRows = document.querySelectorAll(`tr[id*="${matchRow.id}"]`);
+
     for (let i = 0; i < matchRows.length; i++) {
         let row = matchRows[i];
         row = document.getElementById(row.id);
+
         if (row.className.match("accordion-collapse")) {
             row.className = row.className + " show";
         }
+
         let button = row.getElementsByTagName("svg");
+
+        // Update the button image and move the row to the corresponding table
         if (linkType == "link") {
+            // Add to the end of the questionTable if a link was made
             questionTable.getElementsByTagName("tbody")[0].appendChild(row);
             if (button.length > 0) {
                 button = button[0].closest("button");
@@ -516,8 +489,17 @@ function storeLink(event) {
                 getLinkingSVG(button);
             }
         } else if (linkType == "unlink") {
+            // Add to the top of the matchesTable if a link was broken
             const tbody = matchesTable.getElementsByTagName("tbody")[0]
-            tbody.insertBefore(row, tbody.firstChild);
+
+            // The first row should be inserted at the top and all subsequent rows after it
+            if (i == 0) {
+                tbody.insertBefore(row, tbody.firstChild);
+                var headerRow = row;
+            } else {
+                headerRow.after(row);
+            }
+
             if (button.length > 0) {
                 button = button[0].closest("button");
                 button.value = "link";
@@ -528,6 +510,11 @@ function storeLink(event) {
     };
 }
 
+/**
+ * Expand or contract a row
+ * 
+ * @param {*} event Event that triggered expandContractRow 
+ */
 function expandContractRow(event) {
     var element = event.srcElement;
     if (element.tagName == "TD") {
@@ -543,7 +530,6 @@ function expandContractRow(event) {
 export {
     buildTable,
     updateTable,
-    getQuestionData,
     buildSearchTable,
     rowToJSON,
     updateFlashcardTables,
