81e8659facc7d78f054d68de9bf26b24b93733f5
Gilliard Reggie <reggie.gilliard@ohs-dv-adm-rg-l.acf.hhs.local>
2025-05-07 20:42:38 -0400

    Attempting to run dashboard as a shared application

--- code/serve_qa_dashboard.sh
+++ code/serve_qa_dashboard.sh
@@ -1,4 +1,4 @@
-cd ~/repos/ACF-pir-data/
+cd /srv/pir-qa-dashboard/
 source .venv/bin/activate
 google-chrome http://localhost:8080 &>/dev/null &
 waitress-serve --host 127.0.0.1 --call "pir_pipeline.dashboard:create_app"
