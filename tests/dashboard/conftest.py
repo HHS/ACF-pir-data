5ccd8668da119795c120a4bb7d5bb6dc802a40e3
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-05-12 17:18:37 +0000

    Code formatted with black

--- tests/dashboard/conftest.py
+++ tests/dashboard/conftest.py
@@ -15,7 +15,12 @@ from pir_pipeline.dashboard import create_app
 @pytest.fixture(scope="module")
 def app(insert_question_records):
     test_db_config = DB_CONFIG.copy()
-    test_config = {"TESTING": True, "DB_CONFIG": test_db_config, "DB_NAME": "pir_test", "SECRET_KEY": "dev"}
+    test_config = {
+        "TESTING": True,
+        "DB_CONFIG": test_db_config,
+        "DB_NAME": "pir_test",
+        "SECRET_KEY": "dev",
+    }
     app = create_app(
         test_config=test_config,
         template_folder=os.path.join(
