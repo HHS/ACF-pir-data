9e5dc81c7035df0d14ca39292db35cb6be9fe7e7
Gilliard Reggie <reggie.gilliard@ohs-dv-adm-rg-l.acf.hhs.local>
2025-05-12 14:00:06 -0400

    Redeploy sam; revert serve_qa_dashboard
    serve_qa_dashboard was accidentally overwritten, revert
    Redeploy the sam application (i.e. rebuild wheel, requirements etc.)

--- code/serve_qa_dashboard.sh
+++ code/serve_qa_dashboard.sh
@@ -1,4 +1,4 @@
-cd /srv/pir-qa-dashboard/
+cd ~/repos/ACF-pir-data/
 source .venv/bin/activate
 google-chrome http://localhost:8080 &>/dev/null &
 waitress-serve --host 127.0.0.1 --call "pir_pipeline.dashboard:create_app"
