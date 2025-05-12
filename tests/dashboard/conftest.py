92af4ef4ceebd289b80c73d90e97eb472af35107
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-05-12 10:18:23 -0400

    Try using firefox on runner as well

--- tests/dashboard/conftest.py
+++ tests/dashboard/conftest.py
@@ -46,10 +46,11 @@ def runner(app):
 def driver():
     options = Options()
     options.add_argument("--headless")
-    if os.getenv("ON_RUNNER"):
-        driver = webdriver.Chrome(options=options)
-    else:
-        driver = webdriver.Firefox(options=options)
+    driver = webdriver.Firefox(options=options)
+    # if os.getenv("ON_RUNNER"):
+    #     driver = webdriver.Chrome(options=options)
+    # else:
+    #     driver = webdriver.Firefox(options=options)
 
     yield driver
     driver.quit()
