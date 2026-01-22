09c9fa95f86e6f010715cc20803823ad466fa873
Gilliard Reggie <reggie.gilliard@ohs-dv-adm-rg-l.acf.hhs.local>
2026-01-22 11:31:18 -0500

    Tests.dashboard.finalize: Remove driver usage to allow running in cloud

--- tests/dashboard/test_finalize.py
+++ tests/dashboard/test_finalize.py
@@ -1,29 +1,45 @@
 import re
-import time
 
 import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from sqlalchemy import select
 
 
-@pytest.fixture
-def propose_changes(server, driver, sql_utils):
-    wait = WebDriverWait(driver, 10)
-    driver.get("http://127.0.0.1:5000/review")
-    records = sql_utils.get_records(select(sql_utils.tables["flashcard"]))
-    while not records.empty:
-        wait.until(EC.presence_of_element_located((By.ID, "flashcard-question-table")))
-        confirm_button = driver.find_element(By.ID, "confirm-button")
-        wait.until(EC.presence_of_element_located((By.ID, "confirm-button")))
-        confirm_button.click()
-        records = sql_utils.get_records(select(sql_utils.tables["flashcard"]))
-        time.sleep(2)
+@pytest.fixture(scope="class")
+def insert_records(client):
+    input_records = [
+        {
+            f"{i}": {
+                "link_type": "confirm",
+                "base_question_id": f"{i}",
+                "match_question_id": "",
+            }
+        }
+        for i in range(6)
+    ]
+    for record in input_records:
+        with client.session_transaction() as sess:
+            sess["link_dict"] = record
+
+        client.post(
+            "/review/link",
+            json={
+                "action": "store",
+                "html": "",
+            },
+        )
+
+
+@pytest.fixture(scope="class")
+def set_number_displayed(client):
+    with client.session_transaction() as sess:
+        sess["number_displayed"] = 5
 
 
 @pytest.mark.usefixtures(
-    "create_database", "app", "error_message_constructor", "propose_changes"
+    "create_database",
+    "app",
+    "error_message_constructor",
+    "insert_records",
+    "set_number_displayed",
 )
 class TestFinalizeRoutes:
     def test_get_index(self, client):
@@ -38,34 +54,31 @@ class TestFinalizeRoutes:
             returned_title == expected_title
         ), f"Response from finalize did not return expected result, {expected_title}. Instead got {returned_title}."
 
-    def test_post_data(self, client, driver):
-        with client.session_transaction() as sess:
-            sess["number_displayed"] = 5
-
+    def test_post_data(self, client):
         with client:
             # Test if direction is none
             response = client.post("/finalize/data", json={})
             data = response.json
             qid = data["0"]["link_dict"][0]["base_question_id"]
-            assert qid == "5f50241087df4b86810c044c4777566f50ae7453"
+            assert qid == "0"
             assert len(data.keys()) == 5
             # Test if direction is next
             response = client.post("/finalize/data", json={"direction": "next"})
             data = response.json
             qid = data["0"]["link_dict"][0]["base_question_id"]
-            assert qid == "d27e8217ba30000a78e5d92ea54f4d9a2e69cb54"
+            assert qid == "5"
             assert len(data.keys()) == 1
             # Test if direction is previous
             response = client.post("/finalize/data", json={"direction": "previous"})
             data = response.json
             qid = data["0"]["link_dict"][0]["base_question_id"]
-            assert qid == "5f50241087df4b86810c044c4777566f50ae7453"
+            assert qid == "0"
             assert len(data.keys()) == 5
-            # Test if direction is none
+            # Test wrapping
             response = client.post("/finalize/data", json={"direction": "previous"})
             data = response.json
             qid = data["0"]["link_dict"][0]["base_question_id"]
-            assert qid == "d27e8217ba30000a78e5d92ea54f4d9a2e69cb54"
+            assert qid == "5"
             assert len(data.keys()) == 1
 
 
