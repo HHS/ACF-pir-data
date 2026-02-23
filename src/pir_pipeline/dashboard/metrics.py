1566adbd6279305adab8711fb80c69d3b897fef5
Gilliard Reggie <reggie.gilliard@ohs-dv-adm-rg-l.acf.hhs.local>
2026-02-23 08:53:28 -0500

    Metrics: Begin adding metrics page

---
+++ src/pir_pipeline/dashboard/metrics.py
@@ -0,0 +1,23 @@
+"""Routes and logic for the home page"""
+
+from flask import Blueprint, render_template
+
+from pir_pipeline.dashboard.db import get_db
+from pir_pipeline.dashboard.utils import administrator
+
+bp = Blueprint("metrics", __name__, url_prefix="/metrics")
+
+
+@bp.route("/")
+@administrator
+def index():
+    db = get_db()
+    df = db.get_records("SELECT * FROM link_history")
+    approval_by_user = (
+        df.groupby("user")
+        .aggregate(percent_approved=("decision", "mean"))
+        .reset_index()
+        .to_dict(orient="index")
+    )
+
+    return render_template("metrics/metrics.html", approval_by_user=approval_by_user)
