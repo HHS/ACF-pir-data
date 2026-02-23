6ff529dec4dccba2308241a86198a655e44fb284
Gilliard Reggie <reggie.gilliard@ohs-dv-adm-rg-l.acf.hhs.local>
2026-02-23 11:32:51 -0500

    Metrics: Beginning to fill progress page skeleton

--- src/pir_pipeline/dashboard/metrics.py
+++ src/pir_pipeline/dashboard/metrics.py
@@ -13,6 +13,7 @@ bp = Blueprint("metrics", __name__, url_prefix="/metrics")
 def index():
     db = get_db()
     df = db.get_records("SELECT * FROM link_history")
+
     approval_by_user = (
         df.groupby("user")
         .aggregate(percent_approved=("decision", "mean"))
@@ -26,4 +27,17 @@ def index():
 @bp.route("/progress")
 @administrator
 def progress():
-    return render_template("metrics/progress.html")
+    db = get_db()
+    df = db.get_records("SELECT * FROM link_history")
+
+    df["date"] = df["decision_timestamp"].map(lambda stamp: stamp.date())
+    # TODO: Filter to only approved
+    approved = (
+        df.groupby("date")
+        .size()
+        .reset_index()
+        .rename(columns={0: "approved"})
+        .to_dict(orient="index")
+    )
+
+    return render_template("metrics/progress.html", approved=approved)
