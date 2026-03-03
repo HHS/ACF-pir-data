5e743c157616fc664d5e1dad79081a7b1c85f27d
Gilliard Reggie <reggie.gilliard@ohs-dv-adm-rg-l.acf.hhs.local>
2026-03-03 16:38:45 -0500

    Code: New script for serving the dashboard

--- code/serve_qa_dashboard.sh
+++ code/serve_qa_dashboard.sh
@@ -1,5 +1,14 @@
-cd ~/repos/ACF-pir-data/
-source .venv/bin/activate
-export LOADING_DASHBOARD="True"
-google-chrome http://localhost:8080 &>/dev/null &
-waitress-serve --host 127.0.0.1 --call "pir_pipeline.dashboard:create_app"
+source ./application/.venv/bin/activate
+
+USER_NUMBER=$(id -u)
+USER_NAME=$(id -un)
+USER_PORT=$((5000 + $USER_NUMBER % 1000))
+export DB_ENV="production"
+
+if [ $USER_NAME == "reggie.gilliard" ]; then
+    export DB_PROFILE="approver"
+else
+    export DB_PROFILE="reviewer"
+fi
+
+waitress-serve --host 127.0.0.1 --port=${USER_PORT} --call "pir_pipeline.dashboard:create_app" 
